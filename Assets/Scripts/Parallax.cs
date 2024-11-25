9ffd1ad690df7b61f1db9ac20d76e8160be7637c
Aubin EBANO <ebano.aubin@gmail.com>
2024-11-25 16:09:17 +0100

    Parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    private float width;
+    
+    public GameObject camera;
+    public float parallaxRatio;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+        width = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        float dist = camera.transform.position.x * parallaxRatio;
+        float temp = camera.transform.position.x * (1 - parallaxRatio);    
+        
+        transform.position = new Vector3(initialPosition.x + dist, initialPosition.y, initialPosition.z);
+        
+        
+        Debug.Log("temp " + temp);
+        Debug.Log("position " + initialPosition.x + width);
+        if (temp > initialPosition.x + width)
+            initialPosition.x += width;
+        else if (temp < initialPosition.x - width)
+            initialPosition.x -= width;
+    }
+}
